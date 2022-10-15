f7c116125642dbe9783cecc8529e29c4f4da64e6
Adam Warrick <adam.warrick7@gmail.com>
2022-10-15 13:29:33 -0700

    First schema using postgres

---
+++ queries/Postgres query.sql
@@ -0,0 +1,8 @@
+-- Creating talbes for PH-EMployeeDB
+
+CREATE TABLE departments (
+                 dept_no VARCHAR(4) NOT NULL,
+				 dept_name VARCHAR(40) NOT NULL,
+				 PRIMARY KEY (dept_no),
+				 UNIQUE (dept_name)
+);
